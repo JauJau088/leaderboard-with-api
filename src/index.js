702693e11e33a6a44a0d09b423a0d6a6176838bd
jaujau088 <jaujau088@gmail.com>
2022-05-10 13:24:26 +0200

    Add: add score functionality

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ import generateScores from './modules/recent-score-generator.js';
 
 // Generate scores
 generateScores(data);
+
+// Add a score
+const container = document.querySelector('.form');
+const inputName = document.querySelector('#input-name');
+const inputScore = document.querySelector('#input-score');
+
+container.onsubmit = (e) => {
+  e.preventDefault();
+
+  const name = inputName.value;
+  const score = inputScore.value;
+  data.push({ name, score });
+  generateScores(data);
+};
